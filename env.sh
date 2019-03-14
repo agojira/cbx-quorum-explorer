bcee80d6fed4d834a12424d00b635a91dd7ba408
Rodrigo Martínez <dev@brunneis.com>
2019-03-14 15:15:22 +0000

    Update

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
+QUORUM_ENDPOINTS=http://blockchain-pro.westeurope.cloudapp.azure.com:22000,http://blockchain-pro.westeurope.cloudapp.azure.com:22001
 EXTERNAL_API_PORT=443
 API_PORT=18081
 API_DOMAIN=api.arrakis.alastria.councilbox.com
-MONGO_DATA_DIR=/var/explorer-mongodb
-EXPLORER_PORT_2=10083
+MONGO_DATA_DIR=../mongodb
 ENABLE_SSL=true
 EXPLORER_PORT=10081
-QUORUM_ENDPOINTS=http://145.239.197.140:22000,http://51.68.123.147:22000
+EXPLORER_PORT_2=10082
 WEBAPP_VERSION=alastria-arrakis
 WEBAPP_VERSION_2=cbx-alastria-arrakis
